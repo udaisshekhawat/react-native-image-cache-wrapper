ff2935ecba1ac03132bc9a0da7ffae3935177425
Udai Singh Shekhawat <udai.s.shekhawat@gmail.com>
2020-08-08 21:45:34 +0530

    Use react native fetch blob

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import {
 } from 'react-native';
 import React, {Component} from 'react';
 
-import RNFetchBlob from 'rn-fetch-blob';
+import RNFetchBlob from 'react-native-fetch-blob';
 
 const SHA1 = require('crypto-js/sha1');
 
